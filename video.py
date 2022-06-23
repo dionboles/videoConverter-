a2848a7e39f16fbf1bbd7f08a6291323ca0f60fc
Dion Boles <dion.boles@gmail.com>
2022-06-23 14:02:14 +0000

    add main file to script

--- video.py
+++ video.py
@@ -1,20 +1,24 @@
-'''
+"""
  this is a script that loops though a dir and then uses the ffmpeg to change avi to movi and compress it a bit
-'''
+"""
 
-import os 
-if os.path.isdir("video") is False:
-    os.mkdir("video")
-    os.chdir("video")
-else:
-    os.chdir("video")
-
-if os.path.isdir(".") is True:
-   for i in os.listdir('.'):
-       extension = os.path.splitext(i)[1][1:]
-       if extension == "avi":
-           name = str(os.path.splitext(i)[0]).replace("_","");
-           os.system("ffmpeg -i "+i+" "+name+".mov -crf 14");
-         
+import os
 
 
+def main():
+    if os.path.isdir("video") is False:
+        os.mkdir("video")
+        os.chdir("video")
+    else:
+        os.chdir("video")
+
+    if os.path.isdir(".") is True:
+        for i in os.listdir("."):
+            extension = os.path.splitext(i)[1][1:]
+            if extension == "avi":
+                name = str(os.path.splitext(i)[0]).replace("_", "")
+                os.system("ffmpeg -i " + i + " " + name + ".mov -crf 14")
+
+
+if __name__ == "__main__":
+    main()
